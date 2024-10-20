fada5759fa77566f2c0e8912be32c3a82d2db7c6
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-10-20 11:43:04 +0300

    commited

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, render_template , dotenv
+from flask import Flask, jsonify, request, render_template 
 from flask_migrate import Migrate
 from models import db, User, Entry, Photo, Tag 
 import os
